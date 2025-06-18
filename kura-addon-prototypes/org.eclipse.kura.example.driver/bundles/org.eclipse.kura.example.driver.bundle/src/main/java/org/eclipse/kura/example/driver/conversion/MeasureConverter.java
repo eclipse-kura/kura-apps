c5d986dec48e1dc07c371808bded90f02f1c2b07
SimoneFiorani <simone.fiorani@abinsula.com>
2025-06-18 16:25:25 +0200

    fix: comments
    Signed-off-by: SimoneFiorani <simone.fiorani@abinsula.com>

--- kura-addon-prototypes/org.eclipse.kura.example.driver/bundles/org.eclipse.kura.example.driver.bundle/src/main/java/org/eclipse/kura/example/driver/conversion/MeasureConverter.java
+++ kura-addon-prototypes/org.eclipse.kura.example.driver/bundles/org.eclipse.kura.example.driver.bundle/src/main/java/org/eclipse/kura/example/driver/conversion/MeasureConverter.java
@@ -1,6 +1,18 @@
+/*******************************************************************************
+ * Copyright (c) 2025 Eurotech and/or its affiliates and others
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *  Eurotech
+ *******************************************************************************/
+
 package org.eclipse.kura.example.driver.conversion;
 
-import org.eclipse.kura.example.driver.ExampleDriverChannelDescriptor;
 import org.eclipse.kura.example.driver.ExampleDriverChannelDescriptor.UnitMeasure;
 import org.eclipse.kura.type.DataType;
 import org.eclipse.kura.type.TypedValue;
