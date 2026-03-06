c897e9899789ce0bd9680c026de5e88f993b89f6
SimoneFiorani <simone.fiorani@abinsula.com>
2026-03-06 16:22:58 +0100

    test: removing sonar coverage
    Signed-off-by: SimoneFiorani <simone.fiorani@abinsula.com>

--- .jenkins/ci.groovy
+++ .jenkins/ci.groovy
@@ -7,7 +7,7 @@ node {
             enable: true,
             projectKey: "eclipse-kura_kura-apps",
             tokenId: "sonarcloud-token-kura-apps",
-            exclusions: "tests/**/*,**/*.xml,**/*.yml",
+            exclusions: "kura-examples/tests/**/*,kura-addon-prototypes/tests/**/***/*.xml,**/*.yml",
             testExclusions: "**/*"
         ],
     )
