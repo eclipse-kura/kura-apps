564d7f8777a705b0499d5dccdcc66d268bb182c1
SimoneFiorani <simone.fiorani@abinsula.com>
2026-03-10 11:34:53 +0100

    build: removing sonar checks
    Signed-off-by: SimoneFiorani <simone.fiorani@abinsula.com>

--- .jenkins/ci.groovy
+++ .jenkins/ci.groovy
@@ -7,7 +7,7 @@ node {
             enable: true,
             projectKey: "eclipse-kura_kura-apps",
             tokenId: "sonarcloud-token-kura-apps",
-            exclusions: "kura-examples/tests/**/*,kura-addon-prototypes/tests/**/***/*.xml,**/*.yml",
+            exclusions: "**/*",
             testExclusions: "**/*"
         ],
     )
